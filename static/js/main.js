1e353da027145fbe59ff5aa771b96b529aa137ff
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-04-15 13:12:07 +0100

    +fetch data from api

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,8 @@
 const itemSelect = document.querySelector('#itemSelect');
 const templates = document.querySelector('template');
 const itemSelectionTemplate = templates.querySelector('#itemSelectionTemplate');
+
+fetch('/api/all/').then(response => response.json())
+.then(json => {
+    console.log(json);
+})
