9e37771d40ede37815295b87ad968b2bebedbf43
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-04-15 15:39:59 +0100

    +item and component names to item selectors

--- static/js/main.js
+++ static/js/main.js
@@ -2,19 +2,27 @@ class ItemSelector {
     constructor(item) {
         this.itemName = item.name;
         this.components = item.components.filter(component => {
-            let regex = /blueprint|chassis|helmet|systems|barrel|stock|receiver|grip|string|lower limb|upper limb|link| prime|blade|gauntlet|handle|ornament|chain/i;
+            let regex = /blueprint|chassis|neuroptics|systems|barrel|stock|receiver|grip|string|lower limb|upper limb|link| prime|blade|gauntlet|handle|ornament|chain/i;
             return regex.test(component.name);
         });
         this.vaulted = item.hasOwnProperty('vaulted') ? item.hasOwnProperty('vaulted') : false;
         this.element = itemSelectionTemplate.content.cloneNode(true);
+        this.element.querySelector('.itemName').innerText = this.itemName;
         this.element.querySelector('.itemImage').querySelector('img').src = `https://cdn.warframestat.us/img/${item.imageName}`
+        this.components.forEach(component => {
+            let element = itemWrapperTemplate.content.cloneNode(true);
+            element.querySelector('label').htmlFor = component.name;
+            element.querySelector('label').innerText = component.name;
+            element.querySelector('input').name = component.name;
+            this.element.querySelector('.itemOptions').appendChild(element);
+        });
         itemSelect.appendChild(this.element);
     }
 }
 
 const itemSelect = document.querySelector('#itemSelect');
 const itemSelectionTemplate = document.querySelector('#itemSelectionTemplate');
-const itemWrapperTemplate = document.querySelector('#itemWrapperTenplate');
+const itemWrapperTemplate = document.querySelector('#itemWrapperTemplate');
 let itemselectors = [];
 
 fetch('/api/all/').then(response => response.json())
