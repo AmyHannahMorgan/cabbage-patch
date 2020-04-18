24d5192543ed2b24dffb3ddb52f0d4d4b1d753a1
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-04-18 09:07:11 +0100

    ~refactor data storage

--- index.js
+++ index.js
@@ -3,56 +3,50 @@ const axios = require('axios').default;
 const app = express();
 const port = process.env.PORT || 3000;
 
-let warfameData;
-let meleeWeaponData;
-let primaryWeaponData;
-let secondaryWeaponData;
+let apiData = {
+
+}
 
 axios.get('https://raw.githubusercontent.com/WFCD/warframe-items/development/data/json/Warframes.json')
 .then(response => {
-    warfameData = filterPrimes(response.data);
+    apiData.warfameData = filterPrimes(response.data);
 });
 
 axios.get('https://raw.githubusercontent.com/WFCD/warframe-items/development/data/json/Primary.json')
 .then(response => {
-    primaryWeaponData = filterPrimes(response.data);
+    apiData.primaryWeaponData = filterPrimes(response.data);
 });
 
 axios.get('https://raw.githubusercontent.com/WFCD/warframe-items/development/data/json/Secondary.json')
 .then(response => {
-    secondaryWeaponData = filterPrimes(response.data);
+    apiData.secondaryWeaponData = filterPrimes(response.data);
 });
 
 axios.get('https://raw.githubusercontent.com/WFCD/warframe-items/development/data/json/Melee.json')
 .then(response => {
-    meleeWeaponData = filterPrimes(response.data);
+    apiData.meleeWeaponData = filterPrimes(response.data);
 })
 
 app.use(express.static(`${__dirname}/static`));
 
 app.get('/api/warframes', (req, res) => {
-    res.send(warfameData);
+    res.send(apiData.warfameData);
 });
 
 app.get('/api/primary', (req, res) => {
-    res.send(primaryWeaponData);
+    res.send(apiData.primaryWeaponData);
 });
 
 app.get('/api/secondary', (req, res) => {
-    res.send(secondaryWeaponData);
+    res.send(apiData.secondaryWeaponData);
 });
 
 app.get('/api/melee', (req, res) => {
-    res.send(meleeWeaponData);
+    res.send(apiData.meleeWeaponData);
 });
 
 app.get('/api/all', (req, res) => {
-    res.send({
-        warframes: warfameData,
-        primary: primaryWeaponData,
-        secondary: secondaryWeaponData,
-        melee: meleeWeaponData
-    });
+    res.send(apiData);
 })
 
 app.listen(port);
