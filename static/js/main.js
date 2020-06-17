0d8c5f410a16ec75cd55cc0cebc80ce45b982494
AmyHannahMorgan <47041871+AmyHannahMorgan@users.noreply.github.com>
2020-06-17 13:40:09 +0100

    +functionality for get started button

--- static/js/main.js
+++ static/js/main.js
@@ -355,11 +355,16 @@ if(navigator.serviceWorker) {
 
 const tabSelectors = document.querySelectorAll('.tabSelectHolder');
 const tabs = document.querySelectorAll('.tab');
+const GET_STARTED_BUTTON = document.querySelector('#getStartedButton');
 let tabHandlers = [];
 
 for(let i = 0; i < tabSelectors.length; i++) {
     tabHandlers.push(new TabHandler(tabSelectors[i].querySelectorAll('.tabSelect'), tabs));
 }
+
+GET_STARTED_BUTTON.addEventListener('click', () => {
+    document.querySelector(".tabSelect[tabID='itemSelect']").click();
+})
 
 document.addEventListener('scroll', (e) => {
     if(window.scrollY > 0) {
